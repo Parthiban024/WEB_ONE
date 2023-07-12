8c701ce723acae9aeadf8954f7becaeecf8bbc1c
Parthiban024 <parthiban@objectways.com>
2023-07-12 15:06:08 +0530

    first commit

---
+++ www.spring-invest.com/_next/static/chunks/pages/[[...slug]]-b5e877b489620c973fc5.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[120],{7708:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/[[...slug]]",function(){return u(4214)}])}},function(n){n.O(0,[296,388,96,370,774,888,179],(function(){return _=7708,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
